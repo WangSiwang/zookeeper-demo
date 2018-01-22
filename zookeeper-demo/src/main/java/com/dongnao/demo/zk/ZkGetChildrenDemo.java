4dfad1fd148fb8750fdd20fa8be3f372fa5d146f
王思旺 <wangsw@sit.com.cn>
2018-01-22 15:34:29 +0800

    curator  +   zkclient

--- zookeeper-demo/src/main/java/com/dongnao/demo/zk/ZkGetChildrenDemo.java
+++ zookeeper-demo/src/main/java/com/dongnao/demo/zk/ZkGetChildrenDemo.java
@@ -1,7 +1,6 @@
 package com.dongnao.demo.zk;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.zookeeper.CreateMode;
@@ -13,32 +12,30 @@ import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 
-public class ZKChildrenDemo implements Watcher {
-	private static final CountDownLatch cdl = new CountDownLatch(1);
+public class ZkGetChildrenDemo implements Watcher {
 	private static ZooKeeper zk = null;
+	private static CountDownLatch cdl = new CountDownLatch(1);
 
-	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-		zk = new ZooKeeper("192.168.1.129:2181", 5000, new ZKChildrenDemo());
+	public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+		zk = new ZooKeeper("192.168.1.129:2181", 5000, new ZkGetChildrenDemo());
 		cdl.await();
 
-		zk.create("/zk-test", "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-
-		zk.create("/zk-test/c1", "456".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-
-		List<String> list = zk.getChildren("/zk-test", true);
-		for (String str : list)
-			System.out.println(str);
-
-		zk.create("/zk-test/c2", "789".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-
-		Thread.sleep(Integer.MAX_VALUE);
+		String path = "/zk-client";
+		zk.create(path, "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		Thread.sleep(1000);
+		zk.create(path+"/c1", "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		Thread.sleep(1000);
+		zk.delete(path+"/c1", -1);
+		Thread.sleep(1000);
+		zk.delete(path, -1);
+		Thread.sleep(1000);
 	}
 
 	@Override
 	public void process(WatchedEvent event) {
 		if (KeeperState.SyncConnected == event.getState())
 			if (EventType.None == event.getType() && null == event.getPath()) {
-				System.out.println("Zk connected");
+				System.out.println("Zk Connected");
 				cdl.countDown();
 			} else if (event.getType() == EventType.NodeChildrenChanged) {
 				try {
